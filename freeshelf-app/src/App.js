07e1e68fa2b4a3d4b0073625166b5676dcdebd7b
LoxductheGreat <jameelsalhhadin@gmail.com>
2020-10-10 04:47:30 -0400

    needs better css formatting

--- freeshelf-app/src/App.js
+++ freeshelf-app/src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
+import books from './data/books.json'
+import Collapsible from 'react-collapsible'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+function App () {
+  // const title = (
+  //   <div>
+  //     {books.map((book) =>
+  //       <div key={book.id}>{book.title}{book.author}</div>
+  //     )}
+  //   </div>
+  // )
+
+  // function collapse () {
+  //   const [open, setOpen] = useState(false)
+  // }
+
+  const booklist = books.map((book) => (
+    <div key={book.id}>
+      <div className='body'>
+        <div className='title'>{book.title}</div>
+        <div className='author'>{book.author}</div>
+        <div className='des'>{book.shortDescription}</div>
+        <img className='image' src={book.coverImageUrl} />
+        {/* <button on onClick={() => setOpen(!open)}>More Information</button> */}
+        <Collapsible trigger="More Information">
+          <div className='url'><strong>URL: </strong>{book.url}</div>
+          <div className='pub'><strong>Publisher: </strong>{book.publisher}</div>
+          <div className='pub-date'><strong>Publication Date: </strong>{book.publicationDate}</div>
+          <div className='full-des'><strong>Full Description: </strong>{book.detailedDescription}</div>
+        </Collapsible>
+      </div>
     </div>
-  );
-}
+  ))
 
-export default App;
+  return (
+    <div>
+      {booklist}
+    </div>
+  )
+}
+// function App () {
+//   return (
+//     <div>
+//       {books.map((book) => <div key={book.id}>
+//       <div>
+//         <h1 className='title'>{book.title}</h1>
+//         <p>{book.author}</p>
+//       </div>
+//     </div>
+//   )
+// }
+
+
+export default App
